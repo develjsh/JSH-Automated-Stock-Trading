437a8189e95811779ab01c479371d68c37b35331
Jung Seung Hwan <develjsh@gmail.com>
2024-12-23 06:22:13 +0900

    Add SendDiscordStartOfProgram function

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"JSH-Automated-Stock-Trading/config"
 	"JSH-Automated-Stock-Trading/service"
-	"fmt"
 )
 
 var ACCESS_TOKEN string
@@ -25,15 +24,8 @@ func main() {
 	}
 
 	// 매수 전략 설정
-	targetBuyCount := 3                          // 매수할 종목 수
-	buyPercent := 0.33                           // 종목당 매수 금액 비율
-	buyAmount := float64(totalCash) * buyPercent // 종목별 매수 금액 계산
+	targetBuyCount := 3 // 매수할 종목 수
+	buyPercent := 0.33  // 종목당 매수 금액 비율
+	service.SendDiscordStartOfProgram(totalCash, targetBuyCount, buyPercent)
 
-	// 출력
-	fmt.Println("==== 매수 전략 ====")
-	fmt.Printf("총 보유 현금: %d원\n", totalCash)
-	fmt.Printf("종목당 매수 비율: %.2f%%\n", buyPercent*100)
-	fmt.Printf("종목당 매수 금액: %.0f원\n", buyAmount)
-	fmt.Printf("매수할 종목 수: %d개\n", targetBuyCount)
-	fmt.Println("=================")
 }
--- service/send_message.go
+++ service/send_message.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"JSH-Automated-Stock-Trading/config"
 	"bytes"
 	"encoding/json"
 	"fmt"
@@ -9,13 +10,16 @@ import (
 	"time"
 )
 
-func SendMessage(msg string, webhookURL string) {
-	// 현재 시간 가져오기
-	now := time.Now()
+func SendMessage(msg string, webhookURL string) (err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("Send message error", r)
+		}
+	}()
 
 	// 메시지 생성
 	message := map[string]string{
-		"content": fmt.Sprintf("[%s] %s", now.Format("2006-01-02 15:04:05"), msg),
+		"content": msg,
 	}
 
 	// 메시지를 JSON 형식으로 변환
@@ -34,4 +38,39 @@ func SendMessage(msg string, webhookURL string) {
 	// 응답 상태 코드 출력
 	fmt.Printf("Message sent: %s\n", message)
 	fmt.Printf("Response status: %s\n", resp.Status)
+	return nil
+}
+
+func SendDiscordStartOfProgram(totalCash, targetBuyCount int, buyPercent float64) (err error) {
+	loc, err := time.LoadLocation("Asia/Seoul")
+	if err != nil {
+		panic(fmt.Sprintf("Failed to load location: %v", err))
+	}
+
+	// 현재 시간 한국 시간으로 변환
+	now := time.Now().In(loc)
+
+	// 종목당 매수 금액 계산
+	buyAmount := float64(totalCash) * buyPercent
+
+	// 메시지 생성
+	message := fmt.Sprintf(
+		"=== 국내 주식 자동매매 프로그램을 시작합니다 ===\n"+
+			"오늘 날짜 (한국 시간): %s\n"+
+			"==== 매수 전략 ====\n"+
+			"총 보유 현금: %d원\n"+
+			"종목당 매수 비율: %.2f%%\n"+
+			"종목당 매수 금액: %.0f원\n"+
+			"매수할 종목 수: %d개\n"+
+			"=================\n",
+		now.Format("2006-01-02"),
+		totalCash,
+		buyPercent*100,
+		buyAmount,
+		targetBuyCount,
+	)
+	if err := SendMessage(message, config.SetConfig.DiscordWebhookUrl); err != nil {
+		return err
+	}
+	return nil
 }
