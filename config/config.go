6c2723b4f1ab6c7db3d393ee8110303e563926e1
Jung Seung Hwan <develjsh@gmail.com>
2024-12-23 06:37:30 +0900

    Add 프로그램 위치에 따른 시간 설정 함수 추가

--- config/config.go
+++ config/config.go
@@ -1,13 +1,16 @@
 package config
 
 import (
+	"fmt"
 	"log"
 	"os"
+	"time"
 )
 
 var SetConfig Config
 
 type Config struct {
+	Location          string
 	AppKey            string
 	AppSecret         string
 	Cano              string
@@ -18,6 +21,7 @@ type Config struct {
 
 func LoadConfigFromEnv() {
 	SetConfig = Config{
+		Location:          os.Getenv("LOCATION"),
 		AppKey:            os.Getenv("APP_KEY"),
 		AppSecret:         os.Getenv("APP_SECRET"),
 		Cano:              os.Getenv("CANO"),
@@ -26,8 +30,25 @@ func LoadConfigFromEnv() {
 		DiscordWebhookUrl: os.Getenv("DISCORD_WEBHOOK_URL"),
 	}
 
+	// 시간 설정
+	setLocationTime(SetConfig.Location)
+
 	// 필수 환경 변수가 설정되지 않았는지 확인
 	if SetConfig.AppKey == "" || SetConfig.AppSecret == "" {
 		log.Fatal("환경 변수 설정이 올바르지 않습니다. 필수 변수를 확인하세요.")
 	}
+}
+
+func setLocationTime(location string) {
+	if location == "" {
+		location = "Asia/Seoul"
+	}
+
+	loc, err := time.LoadLocation(location)
+	if err != nil {
+		fmt.Printf("Failed to load location: %v, defaulting to Asia/Seoul\n", err)
+		loc, _ := time.LoadLocation("Asia/Seoul")
+		time.Local = loc
+	}
+	time.Local = loc
 }
